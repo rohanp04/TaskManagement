4504450114d39dc6f6374d03cf03d3c8af75fc85
rohanp04 <rohanprajapati2504@gmail.com>
2025-09-28 14:16:32 +0530

    Project initialization

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <h1 class="text-3xl font-bold underline text-center">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
